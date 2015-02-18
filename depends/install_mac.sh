4dc238ee5692452801cef3cde62ad8ef7f2046f1
Eugene Antropov <antigp@gmail.com>
2015-02-18 22:21:13 +0400

    New working install

--- depends/install_mac.sh
+++ depends/install_mac.sh
@@ -2,6 +2,7 @@
 
 cd `dirname $0`
 DEPENDS_DIR=`pwd`
+LIBUSB_SOURCE_DIR=$DEPENDS_DIR/libusb_src
 
 sh ./install_deps.sh
 
@@ -9,6 +10,7 @@ sh ./install_deps.sh
 # libjpeg
 LIBJPEG_SOURCE_DIR=$DEPENDS_DIR/libjpeg_turbo_src
 LIBJPEG_INSTALL_DIR=$DEPENDS_DIR/libjpeg_turbo
+DYLD_LIBRARY_PATH=$DEPENDS_DIR/libjpeg_turbo/lib
 LIBJPEG_VERSION=1.3.1
 
 wget https://downloads.sourceforge.net/project/libjpeg-turbo/$LIBJPEG_VERSION/libjpeg-turbo-$LIBJPEG_VERSION.tar.gz
@@ -19,7 +21,9 @@ cd $LIBJPEG_SOURCE_DIR
 # libjpeg-turbo is missing some files config files (config.guess and config.sub)
 cp $LIBUSB_SOURCE_DIR/config.guess ./
 cp $LIBUSB_SOURCE_DIR/config.sub ./
-./configure --disable-dependency-tracking --host x86_64-apple-darwin --with-jpeg8 --prefix=$LIBJPEG_INSTALL_DIR 
+
+#./configure --disable-dependency-tracking --host x86_64-apple-darwin --with-jpeg8 --prefix=$LIBJPEG_INSTALL_DIR 
+./configure --disable-dependency-tracking --host x86_64-apple-darwin NASM=/usr/local/Cellar/nasm/2.11.06/bin/nasm --with-jpeg8 --prefix=$LIBJPEG_INSTALL_DIR
 make && make install
 
 #get the missing cl.hpp from Khronos.org
